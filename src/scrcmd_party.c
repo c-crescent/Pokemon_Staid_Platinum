3eceb501491e7b062025c17febfc009bd0e797d1
cynthiacrescent <lightredemption@hotmail.com>
2026-08-17 17:22:56 +0700

    Fixed EV reset function

--- src/scrcmd_party.c
+++ src/scrcmd_party.c
@@ -852,12 +852,14 @@ BOOL ScrCmd_ShuffleLeadMonGenetics(ScriptContext *ctx)
         v2 = (v1 & (0x1f << 10)) >> 10;
         Pokemon_SetValue(mon, MON_DATA_SPDEF_IV, &v2);
 
-        Pokemon_SetValue(mon, MON_DATA_HP_EV, 0);
-        Pokemon_SetValue(mon, MON_DATA_ATK_EV, 0);
-        Pokemon_SetValue(mon, MON_DATA_DEF_EV, 0);
-        Pokemon_SetValue(mon, MON_DATA_SPATK_EV, 0);
-        Pokemon_SetValue(mon, MON_DATA_SPDEF_EV, 0);
-        Pokemon_SetValue(mon, MON_DATA_SPEED_EV, 0);
+        u32 zero = 0;
+
+        Pokemon_SetValue(mon, MON_DATA_HP_EV, &zero);
+        Pokemon_SetValue(mon, MON_DATA_ATK_EV, &zero);
+        Pokemon_SetValue(mon, MON_DATA_DEF_EV, &zero);
+        Pokemon_SetValue(mon, MON_DATA_SPATK_EV, &zero);
+        Pokemon_SetValue(mon, MON_DATA_SPDEF_EV, &zero);
+        Pokemon_SetValue(mon, MON_DATA_SPEED_EV, &zero);
         
         Pokemon_CalcStats(mon);
     }   
